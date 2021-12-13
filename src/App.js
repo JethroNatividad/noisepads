fdad5420566d1ce0e3b1ec9eca28e73dea2cd092
JethroSama <kel73896@gmail.com>
2021-12-13 18:13:27 +0800

    Add sample sound

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-
+import sound1 from './sounds/sound1.mp3'
 function App() {
+
+  const pads = [
+    { key: 'q', sound: sound1 },
+    { key: 'w', sound: sound1 }, { key: 'e', sound: sound1 }, { key: 'a', sound: sound1 }, { key: 's', sound: sound1 }, { key: 'd', sound: sound1 }, { key: 'z', sound: sound1 }, { key: 'x', sound: sound1 }, { key: 'c', sound: sound1 }]
   return (
     <div className="App">
       Hello world
