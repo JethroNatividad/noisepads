587c5c45b2eee49a622956aedf2f0fa42fa53ef0
JethroSama <kel73896@gmail.com>
2021-12-13 22:50:51 +0800

    add sound 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import sound1 from './sounds/sound1.mp3'
 import sound2 from './sounds/sound2.mp3'
+import sound3 from './sounds/sound3.mp3'
 import './app.css'
 import Pad from './Pad';
 
@@ -8,7 +9,7 @@ function App() {
 
   const pads = [
     { key: 'q', sound: sound1 },
-    { key: 'w', sound: sound2 }, { key: 'e', sound: sound1 }, { key: 'a', sound: sound1 }, { key: 's', sound: sound1 }, { key: 'd', sound: sound1 }, { key: 'z', sound: sound1 }, { key: 'x', sound: sound1 }, { key: 'c', sound: sound1 }]
+    { key: 'w', sound: sound2 }, { key: 'e', sound: sound3 }, { key: 'a', sound: sound1 }, { key: 's', sound: sound1 }, { key: 'd', sound: sound1 }, { key: 'z', sound: sound1 }, { key: 'x', sound: sound1 }, { key: 'c', sound: sound1 }]
   return (
     <div className="App">
       <div className="PadContainer">
