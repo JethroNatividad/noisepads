c26a98f7fdb65e160d33f254abcb9a2dbaa0404f
JethroSama <kel73896@gmail.com>
2021-12-13 22:03:50 +0800

    styled the pads

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import React from 'react'
 import sound1 from './sounds/sound1.mp3'
+import './app.css'
+import Pad from './Pad';
+
 function App() {
 
   const pads = [
@@ -6,7 +10,9 @@ function App() {
     { key: 'w', sound: sound1 }, { key: 'e', sound: sound1 }, { key: 'a', sound: sound1 }, { key: 's', sound: sound1 }, { key: 'd', sound: sound1 }, { key: 'z', sound: sound1 }, { key: 'x', sound: sound1 }, { key: 'c', sound: sound1 }]
   return (
     <div className="App">
-      Hello world
+      <div className="PadContainer">
+        {pads.map(pad => (<Pad letter={pad.key} key={pad.key} sound={pad.sound} />))}
+      </div>
     </div>
   );
 }
---
+++ src/Pad.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './pad.css'
+const Pad = ({ letter }) => {
+    return (
+        <div className="Pad">
+            {letter}
+        </div>
+    )
+}
+
+export default Pad
