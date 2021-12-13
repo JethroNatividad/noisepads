0be79d744ebd6cec598b1929780e8169ab2125df
JethroSama <kel73896@gmail.com>
2021-12-13 22:35:30 +0800

    change color onclick

--- src/Pad.js
+++ src/Pad.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './pad.css'
 const Pad = ({ letter, sound }) => {
+    const [className, setClassName] = React.useState('Pad')
     const playSound = () => {
+        setClassName('PadClick')
         let audio = new Audio(sound)
         audio.play()
+        setTimeout(() => {
+            setClassName('Pad')
+        }, 100);
     }
     return (
-        <div className="Pad" onClick={playSound}>
+        <div className={className} onClick={playSound}>
             {letter}
         </div>
     )
